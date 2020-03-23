3ff94d24ae24ed8d3c043dac3ad08a2fe05597d1
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-23 21:50:03 +0900

    Add PostList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PostList from "./PostList";
 
 const App = () => {
-    return <div>App</div>
+    return <div className="ui container"><PostList/></div>
 };
 
 export default App;
