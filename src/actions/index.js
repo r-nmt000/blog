8c246bace169e169da953c7a00edaec6ec389f90
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-31 22:32:12 +0900

    Refactor using lodash chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,12 @@ import _ from "lodash";
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    console.log(getState().posts);
 
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(id => dispatch(fetchUser(id)));
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
 };
 
 export const fetchPosts = () => async dispatch => {
